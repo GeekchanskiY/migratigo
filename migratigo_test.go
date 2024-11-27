6e9b1f67a0fa6426073a125060d439c863cb0260
GeekchanskiY <astrovskiy.04@gmail.com>
2024-11-27 12:27:26 +0300

    tests on testcontainers init

--- migratigo_test.go
+++ migratigo_test.go
@@ -1,12 +1,56 @@
 package migratigo
 
 import (
+	"context"
 	"database/sql"
-	"reflect"
+	"embed"
+	"log"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
+//go:embed test_migrations/*
+var testMigrations embed.FS
+
 func TestConnect(t *testing.T) {
+	ctx := context.Background()
+
+	dbName := "users"
+	dbUser := "user"
+	dbPassword := "password"
+
+	postgresContainer, err := postgres.Run(ctx,
+		"postgres:16-alpine",
+		postgres.WithDatabase(dbName),
+		postgres.WithUsername(dbUser),
+		postgres.WithPassword(dbPassword),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+
+	defer func() {
+		if err := testcontainers.TerminateContainer(postgresContainer); err != nil {
+			log.Printf("failed to terminate container: %s", err)
+		}
+	}()
+
+	if err != nil {
+		t.Fatalf("failed to start container: %s", err)
+	}
+
+	connString, err := postgresContainer.ConnectionString(ctx, "sslmode=disable", "application_name=test")
+
+	if err != nil {
+		t.Fatalf("failed to get connection string: %s", connString)
+	}
+
 	type args struct {
 		host     string
 		port     string
@@ -27,20 +71,22 @@ func TestConnect(t *testing.T) {
 				port:     "5432",
 				username: "postgres",
 				password: "postgres",
-				name:     "migratico",
+				name:     "migratigo",
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Connect(tt.args.host, tt.args.port, tt.args.username, tt.args.password, tt.args.name)
+			connection, err := ConnectFromConnectionString(connString)
+			if err != nil {
+				t.Fatalf("failed to connect: %s", err)
+			}
+			connector, err := New(connection, testMigrations)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Connect() error = %v, wantErr %v", err, tt.wantErr)
-				return
+				t.Fatalf("failed to init migratigo: %s", err)
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Connect() got = %v, want %v", got, tt.want)
-			}
+
+			assert.NotNil(t, connector)
 		})
 	}
 }
