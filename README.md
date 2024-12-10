99d5e866b5598adfea5fd4c00dbfd488b08fad37
GeekchanskiY <astrovskiy.04@gmail.com>
2024-12-10 10:33:51 +0300

    cli tools init

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ func main(){
 ```
 
 ## Future plans
- - Downgrade migrations
+ - CLI
  - Auto-create database
  - ORM features
  - Implement database/sql connection interface for connector, maybe
---
+++ cmd/migratigo_cli.go
@@ -0,0 +1,11 @@
+// Package migratigo_cli offers a cli tool for migrations.
+// Available commands are:
+//
+//	migratigo_cli upgrade latest (or version number '001') -- upgrade to latest/selected migration
+//	migratigo_cli downgrade 001 -- downgrade to migration
+//	migratigo_cli init -- create migrations table inside database, requires .env in project dir
+package migratigo_cli
+
+func main() {
+	// TODO: add cli tools
+}
